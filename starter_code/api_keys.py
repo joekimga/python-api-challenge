6a56f952cd614a1de2d13c69ca15b7fab4e6524f
Joseph Kim <joekimga@gmail.com>
2021-02-01 18:41:56 -0500

    work in progress

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0e62da7d2545b9028ed40c830f2ea821"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC5w7_78vuXBjEmV_g6vUCE8o7HqAACHKc"
